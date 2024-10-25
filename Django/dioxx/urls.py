2b4f0d5d4e53e72a1b39aeb40fc7f1ce2e64b6db
ignaciocorrea1 <ig.correar@duocuc.cl>
2024-10-25 02:20:42 -0300

    update

--- Django/dioxx/urls.py
+++ Django/dioxx/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from dioxx import views
 
 urlpatterns = [
+    # General
     path("", views.index, name="index"),
+
+    # Residentes
+    path("listRes/", views.listRes, name="listRes")
+
+    # Personal
+
+    # Emergencias
+
+    # Accesos
+
+    # Medicamentos
+
 ]
--- Django/dioxx/views.py
+++ Django/dioxx/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+""" General """
 def index(request):
     context = {}
     return render(request, "pages/index.html", context)
+
+""" Residentes """
+def listRes(request):
+    context = {}
+    return render(request, "pages/residentes/list_res.html", context)
+
+""" Personal """
+
+""" Emergencias """
+
+""" Accesos """
+
+""" Medicamentos """
