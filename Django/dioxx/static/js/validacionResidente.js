2b0e1176bac72fe5ca34fbb14477354bb79f98f3
unknown <cetecom@AVAWSV03LC1111.duoc.acad>
2024-11-18 09:50:08 -0300

    Avance clases

---
+++ Django/dioxx/static/js/validacionResidente.js
@@ -0,0 +1,17 @@
+/* Formulario add_residente */
+
+function validarAddResidente() {
+    // Campos
+    const rut = document.getElementById("rut").value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const edad = document.getElementById("edad").value;
+    
+    if (rut === "") {
+        console.log("Rut vacio")
+    }
+}
+
+function verjs() {
+    console.log("Si toma")
+}
